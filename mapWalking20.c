85ee89510af252ce874d33120b2be936f2811ce2
João Gabriel Macedo <jgabreumacedo@gmail.com>
2023-06-19 15:07:49 -0300

    map 20 agora com traps

--- mapWalking20.c
+++ mapWalking20.c
@@ -9,19 +9,27 @@
 #define PLAYER_CHAR 'P'
 #define WALL_CHAR '#'
 #define CLEAR_SCREEN system("clear")
+#define TRAP_CHAR 'T'
+
 
 void generateHiddenMap(char map[MAP_SIZE][MAP_SIZE], char hiddenMap[MAP_SIZE][MAP_SIZE], int playerX, int playerY) {
     int i, j;
     for (i = 0; i < MAP_SIZE; i++) {
         for (j = 0; j < MAP_SIZE; j++) {
             if (abs(i - playerY) <= 1 && abs(j - playerX) <= 1) {
-                hiddenMap[i][j] = map[i][j];  /* Mostra uma visão limitada do mapa */
+                hiddenMap[i][j] = map[i][j];  /* Show a limited view of the map */
             } else {
-                hiddenMap[i][j] = UNKNOWN_AREA;  /* Área oculta */
+                hiddenMap[i][j] = UNKNOWN_AREA;  /* Hidden area */
+            }
+
+            /*  Hide the trap if it hasn't been perceived */
+            if (hiddenMap[i][j] == TRAP_CHAR && map[i][j] != TRAP_CHAR) {
+                hiddenMap[i][j] = UNKNOWN_AREA;
             }
         }
     }
 }
+
 
 void printMap(char map[MAP_SIZE][MAP_SIZE], int playerX, int playerY) {
     int i,j;
@@ -44,9 +52,10 @@ int main() {
 
     char hiddenMap[MAP_SIZE][MAP_SIZE];
 
-    int playerX = 1;  /*Posição inicial do jogador*/
+    int playerX = 1;  /* Posição inicial do jogador */
     int playerY = 1;
     int selectedMap;
+    float trapChance, destreza = 10, inteligencia = 10; /* necessário declarar dentro do main ou do menu os valores de destreza e inteligência como float */
     
     if (rand() % 2 == 0) {
         selectedMap = 1;
@@ -82,6 +91,22 @@ int main() {
             case 'D':
                 newX = playerX + 1;
                 break;
+            case 't':
+            case 'T':
+            /*teste de trap*/
+            if (selectedMap == 1) {
+                trapChance = (destreza + inteligencia) * 100 / 20;
+                if (rand() % 100 < trapChance) {
+                    printf("Percebeu uma armadilha!\n");
+                        map1[playerY][playerX] = TRAP_CHAR;
+                    }
+                } else {
+                    trapChance = (destreza + inteligencia) * 100 / 12;
+                    if (rand() % 100 < trapChance) {
+                        printf("Percebeu uma armadilha!\n");
+                        map2[playerY][playerX] = TRAP_CHAR;
+                    }
+                }
             default:
                 printf("Direção inválida! Tente novamente.\n");
                 continue;
