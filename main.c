28669d7fa66a234e6f496eef9a83b0441091c4b1
igormaxlima <igormaxnunes@gmail.com>
2023-06-25 18:50:28 -0300

    adicionando introducao ao main.c

--- main.c
+++ main.c
@@ -14,6 +14,11 @@
 
 int main()
 {
+    printf("\n============================================\n\n");
+    printf("      Seja Bem-Vindo ao Jogo dos Pits!!       \n");
+    printf(" 'Onde suas capacidades serão postas à prova' \n");
+    printf("         numa épica jornada RPG.              \n");
+    printf("\n============================================\n\n");
 
     menu();
 
